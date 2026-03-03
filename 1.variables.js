4142d55716027e7ba383cf55ab30c0bf3440cf69
Utsav Chatrola <uchatrola@gmail.com>
2026-03-03 15:35:45 +0530

    Refactor variable examples and comments

--- 1.variables.js
+++ 1.variables.js
@@ -2,90 +2,88 @@
 
 //1 var
 
-// var name = 'Utsav'
-// //console.log(name)
+var name = 'Utsav'
+console.log(name)  //Utsav
 
-// var name = "Naitikbhai"
-// console.log(name)
+var name = "Naitikbhai"  //value reassign
+console.log(name)  // Naitikbhai
 
 //2 let - block scope
 
-// let age = 22
-// age = 33
+let age = 22
+age = 33
 
-// console.log(age)
+console.log(age) //33
 
 //3 const - block scope & value not change
 
-// const hobby = 'Cricket'
+const hobby = 'Cricket'
 
-// hobby = 'football' //wrong we can not change value and not redeclare the variable with same name
+//hobby = 'football' //wrong we can not change value and not redeclare the variable with same name
 
-// console.log(hobby)
+console.log(hobby)  //cricket
 
 //variable practice
 
-// console.log(fname) //undefined
-// var fname = "utsav"
+console.log(fname) //undefined // variable male che but value nathi malti 
+var fname = "utsav"
+//log undefined because js knows fname is declared but value unable to read
 
-// console.log(name) //referance error
-// const name = 'kano'
+console.log(name) //referance error
+const name = 'kano'
 
-// console.log(names) //referance error
-// let names = 'kano'
+console.log(names) //referance error
+let names = 'kano'
 
 
-// function greet(fname) {
-//   let nam = "Utsav";
-//   function name() {
-//     let lastName = "Patel";
-//     console.log(nam);
-//     function nested() {
-//       lastName = "Chatrola";
-//       console.log(nam + lastName);
-//     }
-//     nested()
-//   }
-//   //console.log(lastName) we can not access the value of lastName bcz it inside the name function
-//   name();
-// }
-// greet();
+function greet(fname) {
+  let nam = "Utsav";
+  function name() {
+    let lastName = "Patel";
+    console.log(nam);
+    function nested() {
+      lastName = "Chatrola";
+      console.log(nam + lastName);
+    }
+    nested()
+  }
+  //console.log(lastName) //we can not access the value of lastName bcz it inside the name function
+  name();
+}
+greet();
 
-// function fullName() {
-//     const firstName = 'Utsav'
-//     function lastName(){
-//         const lName = 'Chatrola'
-//         console.log(firstName+ lName)
-//         function final() {
-//             console.log(lName)
-//         }
-//         final()
-//     }
-//     lastName()
-//     //console.log(lName) not accessible because lastname function no scope ni outside che
-// }
-// fullName()
+function fullName() {
+    const firstName = 'Utsav'
+    function lastName(){
+        const lName = 'Chatrola'
+        console.log(firstName+ lName)
+        function final() {
+            console.log(lName)
+        }
+        final()
+    }
+    lastName()
+    //console.log(lName) not accessible because lastname function no scope ni outside che
+}
+fullName()
 
-// var firstName = "Utsav";
-// function fullName() {
-//   //console.log(firstName)  // undefined because at below var value updates js engine finds var firstName but unable to get values
-//   var firstName = "Kano";
-//   function lastName() {
-//     var lName = "Patel";
-//     function final() {
-//       console.log(firstName + lName);
-//     }
-//     final();
-//   }
-//   lastName();
-//   console.log(firstName);
-//   //console.log(lName); //not accessible because its outside of lastName function scope
-// }
-// fullName();
+var firstName = "Utsav";
+function fullName() {
+  //console.log(firstName)  // undefined because at below var value updates js engine finds var firstName but unable to get values
+  var firstName = "Kano";
+  function lastName() {
+    var lName = "Patel";
+    function final() {
+      console.log(firstName + lName);
+    }
+    final();
+  }
+  lastName();
+  console.log(firstName);
+  //console.log(lName); //not accessible because its outside of lastName function scope
+}
+fullName();
 
-// console.log(xb) //log undefined because js knows xb is declared but value unable to read
-// var xb = 'javascript'
 
-//console.log(isLoggedIn) //it throws referance error because variable declared through let and const not access before declaration
-//const isLoggedIn = 'yes'
-//let isLoggedIn = 'yes'
+
+
